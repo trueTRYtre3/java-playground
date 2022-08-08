fef2955a507030f12a7a707470c8cef2b55dbf4a
Kyle Ortega <kortega@m-c02fq6scmd6m.corp.svbank.com>
2022-08-08 11:57:05 -0700

    new email branch

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -2,6 +2,15 @@ package com.galvanize;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        if (args.length != 2) {
+            if (args.length == 0) {
+                System.out.println("Please specify a name and email");
+            } else if (args.length == 1) {
+                System.out.println("Please specify an email for " + args[0]);
+            }
+            System.exit(1);
+        }
+
+        System.out.println(String.format("%s <%s>", args[0], args[1]));
     }
 }
